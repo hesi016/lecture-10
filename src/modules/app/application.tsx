a1124e40d8ece9b8c3d7702c11fe3f75b35dc84c
Helena Simonsen <hesi016@student.kristiania.no>
2025-03-28 11:40:24 +0100

    exchange OSM background Layer with MapBox background Layer

--- src/modules/app/application.tsx
+++ src/modules/app/application.tsx
@@ -5,12 +5,20 @@ import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
+import { MapboxVectorLayer } from "ol-mapbox-style";
 
 useGeographic();
 
+const osmLayer = new TileLayer({ source: new OSM() });
+const mapBoxLayer = new MapboxVectorLayer({
+  styleUrl: "mapbox://styles/mapbox/bright-v9",
+  accessToken:
+    "pk.eyJ1Ijoiamhhbm5lcyIsImEiOiJjbHVmaHJxcnAwczVyMmpvYzB2aXh6bDI5In0.lrAcWw8waJKbUNyBF8Vzqw",
+});
+
 const map = new Map({
-  view: new View({ center: [10.8, 59.9], zoom: 13 }),
-  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ center: [10.8, 59.9], zoom: 10 }),
+  layers: [mapBoxLayer],
 });
 
 export function Application() {
